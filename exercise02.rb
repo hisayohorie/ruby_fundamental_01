12d5717b1da76535601c5ca2395bd94fa7b633f4
HISAYO <hisayohorie@gmail.com>
2016-03-23 14:35:02 -0400

    up to ex.3

---
+++ exercise02.rb
@@ -0,0 +1,11 @@
+#1
+puts 55 * 0.20 #20% of the entire meal
+
+#2
+puts ("1" + " cat " + "2" + " dogs")
+
+#3
+puts "The answer is #{45628 * 7839}"
+
+#4
+(10 < 20 && 30 < 20) ||!(10 == 11) # => true
