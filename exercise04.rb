8c98d5462548a754403e8f65f2ad9ad6be2ba122
HISAYO <hisayohorie@gmail.com>
2016-03-23 15:39:14 -0400

    done

--- exercise04.rb
+++ exercise04.rb
@@ -1,4 +1,12 @@
-number = 0
-while number <= 100
-  print number += 1
+numbers = (1..100)
+numbers.each do |num|
+  if num % 3 == 0 && num % 5 == 0
+    puts "BitMaker"
+  elsif num % 3 == 0
+    puts "Bits"
+  elsif num % 5 == 0
+    puts "Maker"
+  else
+  puts num
+  end
 end
